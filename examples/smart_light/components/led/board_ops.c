fe98ec9da1623c670b18851946a1e0124cd5ecdd
shixinke <812729686@qq.com>
2020-08-25 21:36:34 +0800

    init

---
+++ examples/smart_light/components/led/board_ops.c
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2020 Tencent Cloud. All rights reserved.
+
+ * Licensed under the MIT License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "board_ops.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/rmt.h"
+#include "led_strip.h"
+
+#define RMT_TX_CHANNEL RMT_CHANNEL_0 
+#define CONFIG_EXAMPLE_RMT_TX_GPIO 18
+#define CONFIG_EXAMPLE_STRIP_LED_NUMBER 24
+
+led_strip_t *strip;
+extern bool wifi_connected;
+
+static void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b)
+{
+    h %= 360; // h -> [0,360]
+    uint32_t rgb_max = v * 2.55f;
+    uint32_t rgb_min = rgb_max * (100 - s) / 100.0f;
+
+    uint32_t i = h / 60;
+    uint32_t diff = h % 60;
+
+    // RGB adjustment amount by hue
+    uint32_t rgb_adj = (rgb_max - rgb_min) * diff / 60;
+
+    switch (i) {
+    case 0:
+        *r = rgb_max;
+        *g = rgb_min + rgb_adj;
+        *b = rgb_min;
+        break;
+    case 1:
+        *r = rgb_max - rgb_adj;
+        *g = rgb_max;
+        *b = rgb_min;
+        break;
+    case 2:
+        *r = rgb_min;
+        *g = rgb_max;
+        *b = rgb_min + rgb_adj;
+        break;
+    case 3:
+        *r = rgb_min;
+        *g = rgb_max - rgb_adj;
+        *b = rgb_max;
+        break;
+    case 4:
+        *r = rgb_min + rgb_adj;
+        *g = rgb_min;
+        *b = rgb_max;
+        break;
+    default:
+        *r = rgb_max;
+        *g = rgb_min;
+        *b = rgb_max - rgb_adj;
+        break;
+    }
+}
+
+void led_set(uint32_t h, uint32_t s, uint32_t v)
+{
+    uint32_t red = 0;
+    uint32_t green = 0;
+    uint32_t blue = 0;
+    led_strip_hsv2rgb(h, s, v, &red, &green, &blue);
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = i; j < CONFIG_EXAMPLE_STRIP_LED_NUMBER; j += 3) {       
+                ESP_ERROR_CHECK(strip->set_pixel(strip, j, red, green, blue));
+            }
+            ESP_ERROR_CHECK(strip->refresh(strip, 10));
+    }
+}
+
+void led_on(uint32_t h, uint32_t s, uint32_t v)
+{
+    led_set(h, s, v);
+}
+
+void led_off(void)
+{
+    ESP_ERROR_CHECK(strip->clear(strip, 10));
+}
+
+static void led_init(void)
+{
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(CONFIG_EXAMPLE_RMT_TX_GPIO, RMT_TX_CHANNEL);
+    // set counter clock to 40MHz
+    config.clk_div = 2;
+
+    ESP_ERROR_CHECK(rmt_config(&config));
+    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
+
+    // install ws2812 driver
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(CONFIG_EXAMPLE_STRIP_LED_NUMBER, (led_strip_dev_t)config.channel);
+    strip = led_strip_new_rmt_ws2812(&strip_config);
+}
+
+void led_task(void* par)
+{
+    uint32_t red = 255;
+    uint32_t green = 0;
+    uint32_t blue = 255;
+    uint16_t hue = 0;
+    uint16_t saturation = 0;
+    uint16_t start_rgb = 0;
+    uint16_t lightness = 0;
+
+    while (!wifi_connected) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = i; j < CONFIG_EXAMPLE_STRIP_LED_NUMBER; j += 3) {       
+                // Build RGB values
+                hue = j * 360 / CONFIG_EXAMPLE_STRIP_LED_NUMBER + start_rgb;
+                led_strip_hsv2rgb(hue, 100, 10, &red, &green, &blue);
+                // Write RGB values to strip driver
+                ESP_ERROR_CHECK(strip->set_pixel(strip, j, red, green, blue));
+            }
+            // Flush RGB values to LEDs
+            ESP_ERROR_CHECK(strip->refresh(strip, 10));
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
+        start_rgb += 1;
+     }
+     led_off();
+     vTaskDelete(NULL);
+}
+
+void board_init(void)
+{
+#ifdef CONFIG_IDF_TARGET_ESP32S2
+    led_init();
+    led_set(100,100,100);
+    xTaskCreate(led_task, "led_task", 2048, NULL, 4, NULL);
+#endif
+}
+
+
